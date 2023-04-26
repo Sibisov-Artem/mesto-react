6a302ed1ff25dc4f011ce74698e96061b78c2873
Artem Sibisov <artsib7777@yandex.ru>
2023-04-26 21:37:12 +0800

    adding imperative handlers: handleEditAvatarClick, handleEditProfileClick, handleAddPlaceClick

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,19 +1,43 @@
 function Main() {
+
+    function handleEditAvatarClick() {
+        document.querySelector('.popup_avatar').classList.add('popup_opened');
+
+    }
+
+    function handleEditProfileClick() {
+        document.querySelector('.popup_profile').classList.add('popup_opened');
+    }
+
+    function handleAddPlaceClick() {
+        document.querySelector('.popup_mesto').classList.add('popup_opened');
+    }
+
     return (
         <main className="content">
 
             <section className="profile section page__section">
-                <div className="profile__avatar-wrapper">
+                <div
+                    onClick={handleEditAvatarClick}
+                    className="profile__avatar-wrapper">
+
                     {/* Вставки вида <% %> — синтаксис шаблона lodash */}
                     <img className="profile__avatar" src="<%=require('./images/profile__avatar.jpg')%>" alt="аватарка" />
                 </div>
 
                 <div className="profile__info">
                     <h1 className="profile__name">Жак-Ив Кусто</h1>
-                    <button className="profile__edit-btn hover" type="button"></button>
+                    <button
+                        onClick={handleEditProfileClick}
+                        className="profile__edit-btn hover"
+                        type="button"
+                    >
+                    </button>
                     <p className="profile__description">Исследователь океана</p>
                 </div>
-                <button className="profile__add-btn hover" type="button"></button>
+                <button
+                    onClick={handleAddPlaceClick}
+                    className="profile__add-btn hover" type="button"></button>
             </section>
 
             <section className="place section page__section" aria-label="Места">
