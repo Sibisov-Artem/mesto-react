7671c1bcd9c09696483f3326a1c523a215fc3ee0
Artem Sibisov <artsib7777@yandex.ru>
2023-05-14 22:08:48 +0800

    fix: after the first reviewer check: delete atribute children in component AddPlacePopup

--- my-app/src/components/AddPlacePopup.js
+++ my-app/src/components/AddPlacePopup.js
@@ -41,26 +41,24 @@ function AddPlacePopup(props) {
             isOpen={props.isOpen}
             onClose={props.onClose}
             onSubmit={handleSubmit}
+        >
 
+            <fieldset className="popup__input-container">
+                <input className="popup__input popup__input_el_mesto-title" type="text" name="name" placeholder="Название"
+                    required minLength="2" maxLength="30" id="mestoTitle"
+                    onChange={handleChangeName}
+                    value={name} />
 
-            children={
-                <fieldset className="popup__input-container">
-                    <input className="popup__input popup__input_el_mesto-title" type="text" name="name" placeholder="Название"
-                        required minLength="2" maxLength="30" id="mestoTitle"
-                        onChange={handleChangeName}
-                        value={name} />
+                <span className="popup__input-error mestoTitle-error"></span>
 
-                    <span className="popup__input-error mestoTitle-error"></span>
+                <input className="popup__input popup__input_el_mesto-url" type="url" name="link" placeholder="Ссылка на картинку"
+                    required id="mestoUrlImage"
+                    onChange={handleChangeLink}
+                    value={link} />
 
-                    <input className="popup__input popup__input_el_mesto-url" type="url" name="link" placeholder="Ссылка на картинку"
-                        required id="mestoUrlImage"
-                        onChange={handleChangeLink}
-                        value={link} />
-
-                    <span className="popup__input-error mestoUrlImage-error"></span>
-                </fieldset>
-            }
-        />
+                <span className="popup__input-error mestoUrlImage-error"></span>
+            </fieldset>
+        </PopupWithForm>
     )
 }
 
