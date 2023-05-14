fb0179dcc7c6de411a21bcee1a08557da2579630
Artem Sibisov <artsib7777@yandex.ru>
2023-05-14 22:42:33 +0800

    fix: after the first reviewer check: in EditProfilePopup, in useEffect add isOpen for insert name and description in input when open popup

--- my-app/src/components/EditProfilePopup.js
+++ my-app/src/components/EditProfilePopup.js
@@ -27,7 +27,7 @@ function EditProfilePopup(props) {
     useEffect(() => {
         setName(currentUser.name);
         setDescription(currentUser.about);
-    }, [currentUser]);
+    }, [currentUser, props.isOpen]);
 
     function handleSubmit(e) {
         // Запрещаем браузеру переходить по адресу формы
